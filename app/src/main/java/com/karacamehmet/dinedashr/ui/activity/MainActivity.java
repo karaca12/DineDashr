78379b19286c408e4273c0c30b3cb8971f0053d1
Mehmet Karaca <mekaraca18@gmail.com>
2023-12-10 20:00:07 +0300

    bitti, toplam 3 bug var çözmeye uğraşmıcam daha

--- app/src/main/java/com/karacamehmet/dinedashr/ui/activity/MainActivity.java
+++ app/src/main/java/com/karacamehmet/dinedashr/ui/activity/MainActivity.java
@@ -1,7 +1,9 @@
 package com.karacamehmet.dinedashr.ui.activity;
 
 import androidx.activity.OnBackPressedCallback;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
@@ -10,9 +12,13 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.karacamehmet.dinedashr.R;
 import com.karacamehmet.dinedashr.databinding.ActivityMainBinding;
+import com.karacamehmet.dinedashr.ui.fragment.BasketFragment;
+import com.karacamehmet.dinedashr.ui.fragment.HomeFragment;
 import com.karacamehmet.dinedashr.ui.viewmodel.BasketViewModel;
 import com.karacamehmet.dinedashr.ui.viewmodel.HomeViewModel;
 
